7380eaeb4dacf3aec259dc4ee8fec2452ea6d19f
ozan aksoy <rockcoden@gmail.com>
2019-05-01 20:11:12 +0300

    [UPDATE]: Simple Comments on Prototype Example of Image Loader

--- src/designpatterns/creational/prototype/imageloader/Prototype.java
+++ src/designpatterns/creational/prototype/imageloader/Prototype.java
@@ -1,8 +1,10 @@
 package designpatterns.creational.prototype.imageloader;
 
+import java.io.Serializable;
+
 /**
  * A simple interface of Prototype Design Pattern on a non-serializable
- * situation. Hence, it does not extend to serializable or clonable for this
+ * situation. Hence, it does not extend to {@link Serializable} or {@link Cloneable} for this
  * condition. However it is assumed that the implementing classes will correctly
  * implement cloning behavior as it is required for the given condition.
  * 
